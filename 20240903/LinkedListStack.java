310d82bf4ed823f785c03942a716caadf6d903a2
nhnk <rlaehdgns4568@gmail.com>
2024-09-03 16:36:18 +0900

    this op add

--- 20240903/LinkedListStack.java
+++ 20240903/LinkedListStack.java
@@ -9,22 +9,22 @@ public class LinkedListStack<E> implements Stackable<E> {
 
     @Override
     public boolean isEmpty() {
-        return list.isEmpty();
+        return this.list.isEmpty();
     }
 
     @Override
     public void push(E element) {
-        list.add(element);
+        this.list.add(element);
     }
 
     @Override
     public E pop() {
-        return list.removeLast();
+        return this.list.removeLast();
     }
 
     @Override
     public E top() {
-        return list.peek();
+        return this.list.peek();
     }
 
 }
