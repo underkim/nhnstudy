29a40ece1d3a1246c356474fd555819d08b4758e
nhnk <rlaehdgns4568@gmail.com>
2024-09-03 16:45:52 +0900

    LinkedListQueue Add

---
+++ 20240903/Queue.java
@@ -0,0 +1,30 @@
+import java.util.LinkedList;
+
+public interface Queue<E> {
+    public void enqueue(E e);
+
+    public E dequeue();
+
+    public int size();
+}
+
+class LinkedListQueue<E> implements Queue<E> {
+
+    LinkedList<E> list;
+
+    @Override
+    public void enqueue(E e) {
+        this.list.add(e);
+    }
+
+    @Override
+    public E dequeue() {
+        return this.list.removeFirst();
+    }
+
+    @Override
+    public int size() {
+        return this.list.size();
+    }
+
+}
